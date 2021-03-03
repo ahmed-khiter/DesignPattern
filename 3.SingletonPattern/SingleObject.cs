36c564b60a7964f4bbd63f4196586d3c3c87947c
ahmed-khiter <ahmed.khaled.atyia@gmail.com>
2021-03-03 22:35:57 +0200

    updates

---
+++ 3.SingletonPattern/SingleObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3.SingletonPattern
+{
+    public class SingleObject
+    {
+        //create an object of SingleObject
+        private static SingleObject instance = new SingleObject();
+
+        //make the constructor private so that this class cannot be
+        //instantiated
+
+        private SingleObject() { }
+
+        //Get the only object available
+
+        public static SingleObject GetInstance()
+        {
+            return instance;
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello World! from SingleObject class");
+        }
+    }
+}
---
+++ DesignPattern/SingletonPatternProgram.cs
@@ -0,0 +1,23 @@
+﻿using _3.SingletonPattern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    class SingletonPatternProgram
+    {
+        static void Main(string[] args)
+        {
+            //illegal construct
+            //Compile Time Error: The constructor SingleObject() is not visible
+            //SingleObject object = new SingleObject();
+
+            //Get the only object available
+            SingleObject singleObject = SingleObject.GetInstance();
+
+            singleObject.ShowMessage();
+
+        }
+    }
+}
